25df607f34f9f847de9028ee4e08cf66f26fc3e9
jahanvi <jahanvigoyal2002@gmail.com>
2023-08-18 12:45:15 +0530

    bottom navigation bar completed

--- lib/ui/common/widgets/bottom_bar.dart
+++ lib/ui/common/widgets/bottom_bar.dart
@@ -1,17 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
-import 'package:leads/ui/views/home/home_viewmodel.dart';
 import 'package:stacked/stacked.dart';
-import 'package:stacked_services/stacked_services.dart';
 
-import '../../../app/app.locator.dart';
+import '/ui/views/home/home_viewmodel.dart';
 import '../images.dart';
 
 class BottomBar extends StackedView<HomeViewModel> {
-  final int currentIndex;
-  BottomBar({Key? key, this.currentIndex = 0}) : super(key: key);
-  final NavigationService _navigationService = locator<NavigationService>();
-
+  const BottomBar({Key? key}) : super(key: key);
   @override
   Widget builder(
     BuildContext context,
@@ -30,14 +25,107 @@ class BottomBar extends StackedView<HomeViewModel> {
                         topLeft: Radius.circular(10),
                         topRight: Radius.circular(10))),
                 child: Row(children: [
-                  bottomItem("Home", 0),
-                  bottomItem("Users", 1),
-                  bottomItem("Leads", 2),
-                  bottomItem("Call Records", 3),
+                  Expanded(
+                      // Home page
+                      child: GestureDetector(
+                          onTap: () {
+                            viewModel.changeIndex(0);
+                          },
+                          child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                SvgPicture.asset(Images().home,
+                                    height: 25,
+                                    width: 25,
+                                    color: viewModel.currentIndex == 0
+                                        ? const Color.fromRGBO(151, 52, 1, 1)
+                                        : const Color.fromRGBO(105, 97, 94, 1)),
+                                Text("HOME",
+                                    style: TextStyle(
+                                        fontSize: 7,
+                                        color: viewModel.currentIndex == 0
+                                            ? const Color.fromRGBO(
+                                                151, 52, 1, 1)
+                                            : const Color.fromRGBO(
+                                                105, 97, 94, 1)))
+                              ]))),
+                  Expanded(
+                      // Users Page
+                      child: GestureDetector(
+                          onTap: () {
+                            viewModel.changeIndex(1);
+                          },
+                          child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                SvgPicture.asset(Images().user,
+                                    height: 23,
+                                    width: 23,
+                                    color: viewModel.currentIndex == 1
+                                        ? const Color.fromRGBO(151, 52, 1, 1)
+                                        : const Color.fromRGBO(105, 97, 94, 1)),
+                                const SizedBox(height: 2),
+                                Text("USERS",
+                                    style: TextStyle(
+                                        fontSize: 7,
+                                        color: viewModel.currentIndex == 1
+                                            ? const Color.fromRGBO(
+                                                151, 52, 1, 1)
+                                            : const Color.fromRGBO(
+                                                105, 97, 94, 1)))
+                              ]))),
+                  Expanded(
+                      // Leads page
+                      child: GestureDetector(
+                          onTap: () {
+                            viewModel.changeIndex(2);
+                          },
+                          child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                SvgPicture.asset(Images().lead,
+                                    height: 25,
+                                    width: 25,
+                                    color: viewModel.currentIndex == 2
+                                        ? const Color.fromRGBO(151, 52, 1, 1)
+                                        : const Color.fromRGBO(105, 97, 94, 1)),
+                                Text("LEADS",
+                                    style: TextStyle(
+                                        fontSize: 7,
+                                        color: viewModel.currentIndex == 2
+                                            ? const Color.fromRGBO(
+                                                151, 52, 1, 1)
+                                            : const Color.fromRGBO(
+                                                105, 97, 94, 1)))
+                              ]))),
+                  Expanded(
+                      // Call Records page
+                      child: GestureDetector(
+                          onTap: () {
+                            viewModel.changeIndex(3);
+                          },
+                          child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                SvgPicture.asset(Images().call,
+                                    height: 25,
+                                    width: 25,
+                                    color: viewModel.currentIndex == 3
+                                        ? const Color.fromRGBO(151, 52, 1, 1)
+                                        : const Color.fromRGBO(105, 97, 94, 1)),
+                                Text("CALL RECORDS",
+                                    style: TextStyle(
+                                        fontSize: 7,
+                                        color: viewModel.currentIndex == 3
+                                            ? const Color.fromRGBO(
+                                                151, 52, 1, 1)
+                                            : const Color.fromRGBO(
+                                                105, 97, 94, 1)))
+                              ]))),
                   const Expanded(child: SizedBox())
                 ]))
           ]),
-          floatingActionButton()
+          floatingActionButton() // Add Button
         ]));
   }
 
@@ -77,38 +165,6 @@ class BottomBar extends StackedView<HomeViewModel> {
                           child: const Icon(Icons.add,
                               size: 28, color: Colors.white))
                     ]))));
-  }
-
-////////////////////////////////////////////////////////// bottom nav bar item widget/////////////////////////////////////////////////////////////////////////////////
-  bottomItem(name, tabIndex) {
-    return Expanded(
-        child: GestureDetector(
-            onTap: () {
-              HomeViewModel().changeIndex(tabIndex);
-            },
-            child:
-                Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-              SvgPicture.asset(
-                  name == "Call Records"
-                      ? Images().call
-                      : name == "Users"
-                          ? Images().user
-                          : name == "Leads"
-                              ? Images().lead
-                              : Images().home,
-                  height: name == "Users" ? 23 : 25,
-                  width: name == "Users" ? 23 : 25,
-                  color: tabIndex == currentIndex
-                      ? const Color.fromRGBO(151, 52, 1, 1)
-                      : const Color.fromRGBO(105, 97, 94, 1)),
-              if (name == "Users") const SizedBox(height: 3),
-              Text(name,
-                  style: TextStyle(
-                      fontSize: 7,
-                      color: tabIndex == currentIndex
-                          ? const Color.fromRGBO(151, 52, 1, 1)
-                          : const Color.fromRGBO(105, 97, 94, 1)))
-            ])));
   }
 
   @override
--- lib/ui/views/home/home_viewmodel.dart
+++ lib/ui/views/home/home_viewmodel.dart
@@ -1,12 +1,8 @@
 import 'package:stacked/stacked.dart';
 
-int currentIndex = 0;
-
 class HomeViewModel extends BaseViewModel {
-  bool isItAlreadyOnTab(index, currentIndex, tabIndex) {
-    return index == tabIndex && currentIndex != tabIndex;
-  }
-
+//change bottom bar index
+  int currentIndex = 0;
   changeIndex(tabindex) {
     currentIndex = tabindex;
     notifyListeners();

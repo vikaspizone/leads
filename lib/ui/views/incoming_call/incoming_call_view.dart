252be0c2592ade3d22e25ad0306b3a45f26deead
Sudhir Kumar <sudhir.kumar@pizoneinfotech.com>
2023-08-19 11:53:32 +0530

    fix incoming call blank sceen issue

--- lib/ui/views/incoming_call/incoming_call_view.dart
+++ lib/ui/views/incoming_call/incoming_call_view.dart
@@ -85,95 +85,92 @@ class IncomingCallView extends StackedView<IncomingCallViewModel>
                             fontWeight: FontWeight.w400),
                       ),
                       verticalSpaceTiny,
-                      Expanded(
-                        child: ListView.builder(
-                          itemCount: viewModel.leadsList.length,
-                          shrinkWrap: true,
-                          physics: const BouncingScrollPhysics(),
-                          padding: const EdgeInsets.symmetric(vertical: 10),
-                          itemBuilder: (context, index) {
-                            return Container(
-                              margin: const EdgeInsets.only(bottom: 15),
-                              child: Container(
-                                decoration: BoxDecoration(
-                                  color: const Color.fromARGB(255, 196, 22, 28),
-                                  borderRadius: BorderRadius.circular(
-                                    10,
-                                  ),
-                                ),
-                                child: SlidableTile(
-                                  action: [
-                                    Expanded(
-                                        child: Container(
-                                      decoration: const BoxDecoration(
-                                        color: Color.fromARGB(255, 196, 22, 28),
-                                        borderRadius: BorderRadius.only(
-                                          topRight: Radius.circular(10),
-                                          bottomRight: Radius.circular(10),
-                                        ),
-                                      ),
-                                      child: Column(
-                                          mainAxisAlignment:
-                                              MainAxisAlignment.center,
-                                          children: [
-                                            SvgPicture.asset(
-                                              'assets/incoming-call-icons/discard-icon.svg',
-                                              height: 20,
-                                            ),
-                                            const Text(
-                                              'Disqualify',
-                                              style: TextStyle(
-                                                  color: Colors.white,
-                                                  fontSize: 12),
-                                            )
-                                          ]),
-                                    )),
-                                    Expanded(
-                                        child: Container(
-                                      decoration: const BoxDecoration(
-                                        color:
-                                            Color.fromARGB(255, 245, 148, 30),
-                                        borderRadius: BorderRadius.only(
-                                          topRight: Radius.circular(10),
-                                          bottomRight: Radius.circular(10),
-                                        ),
-                                      ),
-                                      child: Column(
-                                          mainAxisAlignment:
-                                              MainAxisAlignment.center,
-                                          children: [
-                                            SvgPicture.asset(
-                                              'assets/incoming-call-icons/check-icon.svg',
-                                              height: 20,
-                                            ),
-                                            verticalSpaceTiny,
-                                            const Text(
-                                              'Create\nProspect',
-                                              style: TextStyle(
-                                                  color: Colors.white,
-                                                  fontSize: 12),
-                                              textAlign: TextAlign.center,
-                                            )
-                                          ]),
-                                    )),
-                                  ],
-                                  valueKey: index,
-                                  child: CallDetailCard(
-                                    callDuration:
-                                        viewModel.leadsList[index].callDuration,
-                                    personName:
-                                        viewModel.leadsList[index].personName,
-                                    visitTime:
-                                        viewModel.leadsList[index].visitTime,
-                                    visitDate:
-                                        viewModel.leadsList[index].visitDate,
-                                    status: viewModel.leadsList[index].status,
-                                  ),
+                      ListView.builder(
+                        itemCount: viewModel.leadsList.length,
+                        shrinkWrap: true,
+                        physics: const BouncingScrollPhysics(),
+                        padding: const EdgeInsets.symmetric(vertical: 10),
+                        itemBuilder: (context, index) {
+                          return Container(
+                            margin: const EdgeInsets.only(bottom: 15),
+                            child: Container(
+                              decoration: BoxDecoration(
+                                color: const Color.fromARGB(255, 196, 22, 28),
+                                borderRadius: BorderRadius.circular(
+                                  10,
                                 ),
                               ),
-                            );
-                          },
-                        ),
+                              child: SlidableTile(
+                                action: [
+                                  Expanded(
+                                      child: Container(
+                                    decoration: const BoxDecoration(
+                                      color: Color.fromARGB(255, 196, 22, 28),
+                                      borderRadius: BorderRadius.only(
+                                        topRight: Radius.circular(10),
+                                        bottomRight: Radius.circular(10),
+                                      ),
+                                    ),
+                                    child: Column(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.center,
+                                        children: [
+                                          SvgPicture.asset(
+                                            'assets/incoming-call-icons/discard-icon.svg',
+                                            height: 20,
+                                          ),
+                                          const Text(
+                                            'Disqualify',
+                                            style: TextStyle(
+                                                color: Colors.white,
+                                                fontSize: 12),
+                                          )
+                                        ]),
+                                  )),
+                                  Expanded(
+                                      child: Container(
+                                    decoration: const BoxDecoration(
+                                      color: Color.fromARGB(255, 245, 148, 30),
+                                      borderRadius: BorderRadius.only(
+                                        topRight: Radius.circular(10),
+                                        bottomRight: Radius.circular(10),
+                                      ),
+                                    ),
+                                    child: Column(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.center,
+                                        children: [
+                                          SvgPicture.asset(
+                                            'assets/incoming-call-icons/check-icon.svg',
+                                            height: 20,
+                                          ),
+                                          verticalSpaceTiny,
+                                          const Text(
+                                            'Create\nProspect',
+                                            style: TextStyle(
+                                                color: Colors.white,
+                                                fontSize: 12),
+                                            textAlign: TextAlign.center,
+                                          )
+                                        ]),
+                                  )),
+                                ],
+                                valueKey: index,
+                                child: CallDetailCard(
+                                  callDuration:
+                                      viewModel.leadsList[index].callDuration,
+                                  personName:
+                                      viewModel.leadsList[index].personName,
+                                  visitTime:
+                                      viewModel.leadsList[index].visitTime,
+                                  visitDate:
+                                      viewModel.leadsList[index].visitDate,
+                                  status: viewModel.leadsList[index].status,
+                                ),
+                              ),
+                            ),
+                          );
+                        },
                       ),
 
                       verticalSpaceSmall,
@@ -189,120 +186,118 @@ class IncomingCallView extends StackedView<IncomingCallViewModel>
   }
 
 //Render Select Bar
-  Expanded renderSelectBar(IncomingCallViewModel viewModel) {
-    return Expanded(
-      child: Row(
-        children: [
-          Expanded(
-            flex: 5,
+  Widget renderSelectBar(IncomingCallViewModel viewModel) {
+    return Row(
+      children: [
+        Expanded(
+          flex: 5,
+          child: Container(
+            height: 30,
+            decoration: BoxDecoration(
+                color: const Color(0xFFF9E7C4),
+                borderRadius: BorderRadius.circular(25)),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    viewModel.selectTab('All');
+                  },
+                  child: Container(
+                    alignment: Alignment.center,
+                    height: double.infinity,
+                    padding: const EdgeInsets.symmetric(horizontal: 30),
+                    decoration: BoxDecoration(
+                        color: viewModel.selectedTab == 'All'
+                            ? const Color(0xFFE29C10)
+                            : Colors.transparent,
+                        borderRadius: BorderRadius.circular(25)),
+                    child: Text(
+                      'All',
+                      style: TextStyle(
+                        fontFamily: 'Nexa-Bold',
+                        height: 2,
+                        fontSize: 12,
+                        color: viewModel.selectedTab == 'All'
+                            ? Colors.white
+                            : Colors.black,
+                      ),
+                    ),
+                  ),
+                ),
+                GestureDetector(
+                  onTap: () {
+                    viewModel.selectTab('Identified');
+                  },
+                  child: Container(
+                    alignment: Alignment.center,
+                    height: double.infinity,
+                    padding: const EdgeInsets.symmetric(horizontal: 15),
+                    decoration: BoxDecoration(
+                        color: viewModel.selectedTab == 'Identified'
+                            ? const Color(0xFFE29C10)
+                            : Colors.transparent,
+                        borderRadius: BorderRadius.circular(25)),
+                    child: Text('Identified',
+                        style: TextStyle(
+                            fontFamily: 'Nexa-Bold',
+                            height: 2,
+                            fontSize: 12,
+                            color: viewModel.selectedTab == 'Identified'
+                                ? Colors.white
+                                : Colors.black)),
+                  ),
+                ),
+                GestureDetector(
+                  onTap: () {
+                    viewModel.selectTab('Unidentified');
+                  },
+                  child: Container(
+                    alignment: Alignment.center,
+                    height: double.infinity,
+                    padding: const EdgeInsets.symmetric(horizontal: 15),
+                    decoration: BoxDecoration(
+                        color: viewModel.selectedTab == 'Unidentified'
+                            ? const Color(0xFFE29C10)
+                            : Colors.transparent,
+                        borderRadius: BorderRadius.circular(25)),
+                    child: Text(
+                      'Unidentified',
+                      style: TextStyle(
+                        fontFamily: 'Nexa-Bold',
+                        height: 2,
+                        fontSize: 12,
+                        color: viewModel.selectedTab == 'Unidentified'
+                            ? Colors.white
+                            : Colors.black,
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+        horizontalSpaceSmall,
+        Expanded(
+            flex: 1,
             child: Container(
               height: 30,
               decoration: BoxDecoration(
-                  color: const Color(0xFFF9E7C4),
+                  color: const Color(0xFFE8E8E8),
                   borderRadius: BorderRadius.circular(25)),
               child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                mainAxisAlignment: MainAxisAlignment.end,
                 children: [
-                  GestureDetector(
-                    onTap: () {
-                      viewModel.selectTab('All');
-                    },
-                    child: Container(
-                      alignment: Alignment.center,
-                      height: double.infinity,
-                      padding: const EdgeInsets.symmetric(horizontal: 30),
-                      decoration: BoxDecoration(
-                          color: viewModel.selectedTab == 'All'
-                              ? const Color(0xFFE29C10)
-                              : Colors.transparent,
-                          borderRadius: BorderRadius.circular(25)),
-                      child: Text(
-                        'All',
-                        style: TextStyle(
-                          fontFamily: 'Nexa-Bold',
-                          height: 2,
-                          fontSize: 12,
-                          color: viewModel.selectedTab == 'All'
-                              ? Colors.white
-                              : Colors.black,
-                        ),
-                      ),
-                    ),
+                  SvgPicture.asset(
+                    Images().menuIcon,
+                    color: const Color.fromARGB(255, 80, 80, 80),
                   ),
-                  GestureDetector(
-                    onTap: () {
-                      viewModel.selectTab('Identified');
-                    },
-                    child: Container(
-                      alignment: Alignment.center,
-                      height: double.infinity,
-                      padding: const EdgeInsets.symmetric(horizontal: 15),
-                      decoration: BoxDecoration(
-                          color: viewModel.selectedTab == 'Identified'
-                              ? const Color(0xFFE29C10)
-                              : Colors.transparent,
-                          borderRadius: BorderRadius.circular(25)),
-                      child: Text('Identified',
-                          style: TextStyle(
-                              fontFamily: 'Nexa-Bold',
-                              height: 2,
-                              fontSize: 12,
-                              color: viewModel.selectedTab == 'Identified'
-                                  ? Colors.white
-                                  : Colors.black)),
-                    ),
-                  ),
-                  GestureDetector(
-                    onTap: () {
-                      viewModel.selectTab('Unidentified');
-                    },
-                    child: Container(
-                      alignment: Alignment.center,
-                      height: double.infinity,
-                      padding: const EdgeInsets.symmetric(horizontal: 15),
-                      decoration: BoxDecoration(
-                          color: viewModel.selectedTab == 'Unidentified'
-                              ? const Color(0xFFE29C10)
-                              : Colors.transparent,
-                          borderRadius: BorderRadius.circular(25)),
-                      child: Text(
-                        'Unidentified',
-                        style: TextStyle(
-                          fontFamily: 'Nexa-Bold',
-                          height: 2,
-                          fontSize: 12,
-                          color: viewModel.selectedTab == 'Unidentified'
-                              ? Colors.white
-                              : Colors.black,
-                        ),
-                      ),
-                    ),
-                  )
+                  const Icon(Icons.arrow_drop_down, color: Colors.black26)
                 ],
               ),
-            ),
-          ),
-          horizontalSpaceSmall,
-          Expanded(
-              flex: 1,
-              child: Container(
-                height: 30,
-                decoration: BoxDecoration(
-                    color: const Color(0xFFE8E8E8),
-                    borderRadius: BorderRadius.circular(25)),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    SvgPicture.asset(
-                      Images().menuIcon,
-                      color: const Color.fromARGB(255, 80, 80, 80),
-                    ),
-                    const Icon(Icons.arrow_drop_down, color: Colors.black26)
-                  ],
-                ),
-              ))
-        ],
-      ),
+            ))
+      ],
     );
   }
 
